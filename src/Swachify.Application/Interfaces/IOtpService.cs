e55c2da4428328b92c264f881c021ceb8eab879d
Dudekula Khasim <khasim.dudekula@rm1codershub.com>
2025-10-20 07:44:53 +0530

    implemented iotp service

---
+++ src/Swachify.Application/Interfaces/IOtpService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swachify.Application.Interfaces
+{
+    public interface IOtpService
+    {
+        Task<bool> SendOtpAsync(string phoneNumber);
+        Task<bool> VerifyOtpAsync(string phoneNumber, string code);
+    }
+}
